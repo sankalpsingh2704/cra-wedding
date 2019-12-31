549fdc78d50c7564377f355da9c7143a81c3086a
sankalpnarayan27 <56334939+sankalpnarayan27@users.noreply.github.com>
2019-12-31 16:25:54 +0530

    Updates

---
+++ precache-manifest.328d51fb528702c8cee057eb9ae7e9b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc40b35554eb7c7c07e7ae73d55d0c4",
+    "url": "/cra-wedding/index.html"
+  },
+  {
+    "revision": "b7f563900a79d498fa8b",
+    "url": "/cra-wedding/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0ee3d019a96296062728",
+    "url": "/cra-wedding/static/js/2.38b4e539.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cra-wedding/static/js/2.38b4e539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7f563900a79d498fa8b",
+    "url": "/cra-wedding/static/js/main.66e8d83b.chunk.js"
+  },
+  {
+    "revision": "bc230dff77a1560d6b26",
+    "url": "/cra-wedding/static/js/runtime-main.20124379.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/cra-wedding/static/media/logo.ee7cd8ed.svg"
+  }
+]);
